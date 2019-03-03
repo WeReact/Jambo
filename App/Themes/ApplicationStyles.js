27ed7b741695e8fb963c7f6d44fcabf49d7c4bd5
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2019-03-03 16:07:33 -0300

    fixing some styles.

--- App/Themes/ApplicationStyles.js
+++ App/Themes/ApplicationStyles.js
@@ -1,45 +1,45 @@
-import { Platform } from 'react-native'
-import { Metrics, Colors, Fonts } from '../Themes'
+import { Platform } from 'react-native';
+import { Metrics, Colors, Fonts } from '../Themes';
 
 const ApplicationStyles = {
-  screen: {
-    mainContainer: {
-      flex: 1,
-      backgroundColor: Colors.transparent
-    },
-    safeArea: {
-      flex: 1,
-      backgroundColor: Colors.transparent
-    },
-    backgroundImage: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0
-    },
-    container: {
-      flex: 1,
-      paddingTop: Metrics.baseMargin,
-      backgroundColor: Colors.transparent
-    },
-    headerTitle: {
-      ...Fonts.style.headerTitle,
-      color: Colors.dark,
-      ...Platform.select({
-        android: {
-          marginLeft: 18
-        }
-      })
-    },
-    floatBackButton: {
-      ...Metrics.signInFloatBackButton,
-      position: 'absolute'
-    },
-    floatBackIcon: {
-      ...Metrics.signInFloatBackIcon
-    }
-  }
-}
+	screen: {
+		mainContainer: {
+			flex: 1,
+			backgroundColor: Colors.white
+		},
+		safeArea: {
+			flex: 1,
+			backgroundColor: Colors.transparent
+		},
+		backgroundImage: {
+			position: 'absolute',
+			top: 0,
+			left: 0,
+			bottom: 0,
+			right: 0
+		},
+		container: {
+			flex: 1,
+			paddingTop: Metrics.baseMargin,
+			backgroundColor: Colors.transparent
+		},
+		headerTitle: {
+			...Fonts.style.headerTitle,
+			color: Colors.dark,
+			...Platform.select({
+				android: {
+					marginLeft: 18
+				}
+			})
+		},
+		floatBackButton: {
+			...Metrics.signInFloatBackButton,
+			position: 'absolute'
+		},
+		floatBackIcon: {
+			...Metrics.signInFloatBackIcon
+		}
+	}
+};
 
-export default ApplicationStyles
+export default ApplicationStyles;
